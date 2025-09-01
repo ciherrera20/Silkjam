40339dc035ca5a1d9b0739936de41049e212a8b9
Chris Herrera <ciherrera20@icloud.com>
2025-09-01 00:02:05 -0700

    Re-organized orchestrator code

--- app/orchestrator/core/orchestrator.py
+++ app/orchestrator/core/orchestrator.py
@@ -20,6 +20,8 @@ MINECRAFT_PORT = os.environ.get("MINECRAFT_PORT", 25565)
 class MCOrchestrator(Supervisor):
     def __init__(self, root: str | Path):
         super().__init__()
+        self.log = PrefixLoggerAdapter(logger, 'orchestrator')
+
         self.root = Path(root)
 
         # Proxy and server listings
@@ -30,12 +32,11 @@ class MCOrchestrator(Supervisor):
         self.proxies = {}  # Proxy name -> MCProxy object
         self.backends = {}  # Server name -> MCBackend object
 
-        self.log = PrefixLoggerAdapter(logger, 'orchestrator')
-
     async def _start(self):
         await super()._start()
 
         # Open and read config file
+        self._config_changed.set()
         self.config_file = self.stack.enter_context((self.root / "config.json").open("a+"))
         self.config_file.seek(0)
         try:
@@ -59,59 +60,61 @@ class MCOrchestrator(Supervisor):
             json.dump(self.config, self.config_file, indent=4)
             self.config_file.flush()
 
+    def reload_config(self):
+        # Start proxies in the listing that are not currently running
+        proxy_listing_names = set()
+        for listing in self.config["proxy_listing"]:
+            name = listing["name"]
+            proxy_listing_names.add(name)
+            if name not in self.proxies:
+                self.log.info("Found proxy listing entry %s", name)
+                proxy = MCProxy(listing)
+                self.proxies[name] = proxy
+                self.add_unit(proxy, proxy.serve_forever)
+
+        # Cleanup any proxies in the listing that no longer exist
+        removed_proxy_names = set()
+        for name, proxy in self.proxies.items():
+            if name not in proxy_listing_names:
+                self.remove_unit_nowait(proxy)
+                removed_proxy_names.add(name)
+        for name in removed_proxy_names:
+            del self.proxies[name]
+
+        # Start servers in the listing that are not currently running
+        backend_names = set()
+        for listing in self.config["server_listing"]:
+            name = listing["name"]
+            backend_names.add(name)
+            if name not in self.backends:
+                self.log.info("Found server listing entry %s", name)
+                backend = MCBackend(self.root / name, listing)
+                self.backends[name] = backend
+                self.add_unit(backend, backend.serve_forever)
+
+        # Cleanup any servers in the listing that no longer exist
+        removed_backend_names = set()
+        for name, backend in self.backends.items():
+            if name not in backend_names:
+                self.remove_unit_nowait(backend)
+                removed_backend_names.add(name)
+        for name in removed_backend_names:
+            del self.backends[name]
+
+        # Update all proxy backends
+        proxy_backends = defaultdict(list)
+        for listing in self.config["server_listing"]:
+            proxy_backends[listing["proxy"]].append(self.backends[listing["name"]])
+        for name, proxy in self.proxies.items():
+            proxy.backends = proxy_backends[name]
+
     async def run_servers(self):
         while True:
-            # Start proxies in the listing that are not currently running
-            proxy_listing_names = set()
-            for listing in self.config["proxy_listing"]:
-                name = listing["name"]
-                proxy_listing_names.add(name)
-                if name not in self.proxies:
-                    self.log.info("Found proxy listing entry %s", name)
-                    proxy = MCProxy(listing)
-                    self.proxies[name] = proxy
-                    self.add_unit(proxy, proxy.serve_forever)
-
-            # Cleanup any proxies in the listing that no longer exist
-            removed_proxy_names = set()
-            for name, proxy in self.proxies.items():
-                if name not in proxy_listing_names:
-                    self.remove_unit_nowait(proxy)
-                    removed_proxy_names.add(name)
-            for name in removed_proxy_names:
-                del self.proxies[name]
-
-            # Start servers in the listing that are not currently running
-            backend_names = set()
-            for listing in self.config["server_listing"]:
-                name = listing["name"]
-                backend_names.add(name)
-                if name not in self.backends:
-                    self.log.info("Found server listing entry %s", name)
-                    backend = MCBackend(self.root / name, listing)
-                    self.backends[name] = backend
-                    self.add_unit(backend, backend.serve_forever)
-
-            # Cleanup any servers in the listing that no longer exist
-            removed_backend_names = set()
-            for name, backend in self.backends.items():
-                if name not in backend_names:
-                    self.remove_unit_nowait(backend)
-                    removed_backend_names.add(name)
-            for name in removed_backend_names:
-                del self.backends[name]
-
-            # Update all proxy backends
-            proxy_backends = defaultdict(list)
-            for listing in self.config["server_listing"]:
-                proxy_backends[listing["proxy"]].append(self.backends[listing["name"]])
-            for name, proxy in self.proxies.items():
-                proxy.backends = proxy_backends[name]
-
             # Wait until the config changes or a proxy or server task is canceled or errors out
-            (done_events, _), (_, _) = await self.supervise_until([self._config_changed])
+            (done_events, _), (_, _) = await self.supervise_until([self._config_changed], return_when=Supervisor.FIRST_EVENT)
             if self._config_changed in done_events:
                 self.log.debug("Config change requested")
+                self.reload_config()
                 self._config_changed.clear()
 
     async def _stop(self, *args):
