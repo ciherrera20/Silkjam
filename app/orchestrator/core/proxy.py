a8538df2028c66a06a8453d154b17d04d5b79cbe
Chris Herrera <ciherrera20@icloud.com>
2025-09-06 03:19:24 -0700

    Added connection IP and port to logs

--- app/orchestrator/core/proxy.py
+++ app/orchestrator/core/proxy.py
@@ -51,7 +51,8 @@ class MCProxy(BaseAsyncContextManager):
 
     async def _identify_backend(
             self,
-            packet_reader: PacketReader
+            packet_reader: PacketReader,
+            conn_logger: logging.Logger | logging.LoggerAdapter
         ) -> tuple[
             MCBackend | None,
             dict | None,
@@ -63,56 +64,64 @@ class MCProxy(BaseAsyncContextManager):
                 # Try parsing as a legacy ping
                 handshake = await packet_reader.read_legacy_ping()
                 is_legacy_ping = True
-                self.log.debug("Received legacy ping: %s", handshake)
+                conn_logger.debug("Received legacy ping: %s", handshake)
                 server_address = handshake["hostname"]
                 if server_address[-len(HOSTNAME):] == HOSTNAME:
                     subdomain = server_address.split(HOSTNAME)[0][:-1]
-                    self.log.debug("Identified subdomain: %s", subdomain)
+                    conn_logger.debug("Identified subdomain: %s", subdomain)
                     if subdomain in self.subdomain_map:
                         # Backend identified with legacy ping
                         backend = self.subdomain_map[subdomain]
-                        self.log.debug("Found server %s at %s", backend.name, server_address)
+                        conn_logger.debug("Found server %s at %s", backend.name, server_address)
                     else:
-                        self.log.debug("No server found at %s", server_address)
+                        conn_logger.debug("No server found at %s", server_address)
                 else:
-                    self.log.debug("Could not find hostname %s in server address %s", HOSTNAME, server_address)
+                    conn_logger.debug("Could not find hostname %s in server address %s", HOSTNAME, server_address)
             except MCProtocolError as err:
                 # Handle modern handshake
                 try:
                     # Read initial handshake packet
                     handshake = await packet_reader.read_handshake_packet()
                     is_legacy_ping = False
-                    self.log.debug("Received handshake: %s", handshake)
+                    conn_logger.debug("Received handshake: %s", handshake)
                     server_address = handshake["server_address"]
                     if server_address[-len(HOSTNAME):] == HOSTNAME:
                         subdomain = server_address.split(HOSTNAME)[0][:-1]
-                        self.log.debug("Identified subdomain: %s", subdomain)
+                        conn_logger.debug("Identified subdomain: %s", subdomain)
                         if subdomain in self.subdomain_map:
                             # Backend identified with modern handshake
                             backend = self.subdomain_map[subdomain]
-                            self.log.debug("Found server %s at %s", backend.name, server_address)
+                            conn_logger.debug("Found server %s at %s", backend.name, server_address)
                         else:
-                            self.log.debug("No server found at %s", server_address)
+                            conn_logger.debug("No server found at %s", server_address)
                     else:
-                        self.log.debug("Could not find hostname %s in server address %s", HOSTNAME, server_address)
+                        conn_logger.debug("Could not find hostname %s in server address %s", HOSTNAME, server_address)
                 except MCProtocolError as err:
-                    self.log.debug("Error during handshake: %s", err)
+                    conn_logger.debug("Error during handshake: %s", err)
                 except ConnectionResetError:
-                    self.log.info("Client closed connection unexpectedly")
+                    conn_logger.info("Client closed connection unexpectedly")
         except Exception as err:
-            self.log.exception("Exception caught while identifying backend: %s", err)
+            conn_logger.exception("Exception caught while identifying backend: %s", err)
         return backend, handshake, is_legacy_ping
 
-    async def _handle_handshake(self, backend, handshake, is_legacy_ping, packet_reader: PacketReader, packet_writer: PacketWriter):
+    async def _handle_handshake(
+            self,
+            backend: MCBackend,
+            handshake: dict,
+            is_legacy_ping: bool,
+            packet_reader: PacketReader,
+            packet_writer: PacketWriter,
+            conn_logger: logging.Logger | logging.LoggerAdapter
+        ):
         try:
             if is_legacy_ping:
                 # Respond to legacy ping
-                self.log.debug("Responding to client legacy ping")
+                conn_logger.debug("Responding to client legacy ping")
                 packet_writer.write_legacy_ping_response(backend.version.protocol, backend.version.name, backend.motd, backend.max_players)
                 await packet_writer.drain()
             else:
                 # Respond to modern handshake
-                self.log.debug("Responding to client handshake")
+                conn_logger.debug("Responding to client handshake")
                 try:
                     if handshake["next_state"] == 1:
                         # Read request packet and respond
@@ -147,7 +156,7 @@ class MCProxy(BaseAsyncContextManager):
                             await packet_writer.drain()
                     elif handshake["next_state"] == 2:
                         # Backend server is starting, respond with message telling the client to wait
-                        self.log.info("Backend server %s not ready yet, sending waking kick message", backend.name)
+                        conn_logger.info("Backend server %s not ready yet, sending waking kick message", backend.name)
                         kick_payload = {
                             "text": backend.waking_kick_msg
                         }
@@ -156,11 +165,11 @@ class MCProxy(BaseAsyncContextManager):
                     else:
                         raise MCProtocolError(f"Unknown next state in handshake: {handshake['next_state']}")
                 except MCProtocolError as err:
-                    self.log.debug("Error during handshake: %s", err)
+                    conn_logger.debug("Error during handshake: %s", err)
                 except ConnectionResetError:
-                    self.log.info("Client closed connection unexpectedly")
+                    conn_logger.info("Client closed connection unexpectedly")
         except Exception as err:
-            self.log.exception("Exception caught while handling client handshake: %s", err)
+            conn_logger.exception("Exception caught while handling client handshake: %s", err)
 
     async def _forward_to_backend(
             self,
@@ -169,15 +178,16 @@ class MCProxy(BaseAsyncContextManager):
             is_legacy_ping: bool,
             packet_reader: PacketReader,
             packet_writer: PacketWriter,
+            conn_logger: logging.Logger | logging.LoggerAdapter
         ):
         # Forward traffic to actual minecraft server
-        self.log.debug("Starting port forwarding to %s", backend.name)
+        conn_logger.debug("Starting port forwarding to %s", backend.name)
         try:
             # Try connecting to the backend server
             backend_reader, backend_writer = await asyncio.open_connection("0.0.0.0", backend.port)
         except ConnectionRefusedError as err:
             # Backend server should be ready but is refusing connections
-            self.log.error("Backend server %s should be ready, but is not accepting connections", backend.name)
+            conn_logger.error("Backend server %s should be ready, but is not accepting connections", backend.name)
             kick_payload = {
                 "text": f"§4Error connecting to {backend.name}"
             }
@@ -185,11 +195,11 @@ class MCProxy(BaseAsyncContextManager):
             await packet_writer.drain()
         else:
             if is_legacy_ping:
-                self.log.debug("Forwarding legacy ping to backend")
+                conn_logger.debug("Forwarding legacy ping to backend")
                 initial_data = packet_writer.encode_legacy_ping(**handshake)
                 player_joining = False
             else:
-                self.log.debug("Forwarding handshake to backend")
+                conn_logger.debug("Forwarding handshake to backend")
                 initial_data = packet_writer.encode_handshake_packet(**handshake)
                 player_joining = handshake["next_state"] == 2
             initial_data += packet_reader.unparsed.tobytes()
@@ -199,9 +209,9 @@ class MCProxy(BaseAsyncContextManager):
             try:
                 async def forward(initial_data, src_reader, dst_writer, direction_msg=None):
                     if direction_msg:
-                        forwarding_logger = PrefixLoggerAdapter(direction_msg) | self.log
+                        forwarding_logger = PrefixLoggerAdapter(direction_msg) | conn_logger
                     else:
-                        forwarding_logger = self.log
+                        forwarding_logger = conn_logger
                     try:
                         if len(initial_data) > 0:
                             dst_writer.write(initial_data)
@@ -221,7 +231,7 @@ class MCProxy(BaseAsyncContextManager):
                     forward(b"", backend_reader, packet_writer.writer, f"{backend.name} -> client"), # backend -> proxy -> client
                 )
             except Exception as err:
-                self.log.exception("Exception caught while forwarding to backend: %s", err)
+                conn_logger.exception("Exception caught while forwarding to backend: %s", err)
             finally:
                 backend_writer.close()
                 await backend_writer.wait_closed()
@@ -229,26 +239,31 @@ class MCProxy(BaseAsyncContextManager):
                 backend.decr_online_players()
 
     async def _handle_client(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-        logger.debug("Client connected")
         try:
+            ip, port = writer.get_extra_info("peername")
+            conn_logger = PrefixLoggerAdapter(f"{ip}:{port}") | self.log
+            conn_logger.debug("Client connected")
+
             packet_reader = PacketReader(reader, timeout=30)
             packet_writer = PacketWriter(writer, timeout=30)
-            backend, handshake, is_legacy_ping = await self._identify_backend(packet_reader)
+            backend, handshake, is_legacy_ping = await self._identify_backend(packet_reader, conn_logger)
             if backend is not None:
                 if not backend.mcproc_running():
                     # Start server if a player is trying to join
                     player_joining = is_legacy_ping or handshake["next_state"] == 2
                     if not backend.mcproc_starting() and player_joining:
-                        self.log.info("Starting backend server %s", backend.name)
+                        conn_logger.info("Starting backend server %s", backend.name)
                         backend.start_mcproc()  # Start actual server process
 
                     # Respond to client while server is sleeping
-                    await self._handle_handshake(backend, handshake, is_legacy_ping, packet_reader, packet_writer)
+                    await self._handle_handshake(backend, handshake, is_legacy_ping, packet_reader, packet_writer, conn_logger)
                 else:
                     # Backend server is running, forward packets to it
-                    await self._forward_to_backend(backend, handshake, is_legacy_ping, packet_reader, packet_writer)
+                    await self._forward_to_backend(backend, handshake, is_legacy_ping, packet_reader, packet_writer, conn_logger)
+            else:
+                conn_logger.debug("Could not identify backend, closing connection")
         except Exception as err:
-            self.log.exception("Exception caught while handling client: %s", err)
+            conn_logger.exception("Exception caught while handling client: %s", err)
         finally:
             writer.close()
             await writer.wait_closed()
