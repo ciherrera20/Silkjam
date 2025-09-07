150a5f9f963d3e385ed4a586defd0ecdad1f35f5
Chris Herrera <ciherrera20@icloud.com>
2025-09-07 02:19:12 -0700

    Added semantic validation to config model

--- app/orchestrator/models/config.py
+++ app/orchestrator/models/config.py
@@ -1,11 +1,22 @@
 import json
 from pathlib import Path
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, PrivateAttr, model_validator, computed_field
 
 class ProxyListing(BaseModel):
     name: str
     port: int
     enabled: bool
+
+    # Annotate listing as valid or not
+    _errors: list[str] = PrivateAttr(default_factory=list)
+
+    @property
+    def valid(self):
+        return len(self._errors) == 0
+
+    @property
+    def errors(self):
+        return self._errors
 
 class SleepProperties(BaseModel):
     timeout: int | None = None
@@ -16,17 +27,56 @@ class Version(BaseModel):
     name: str
     protocol: int
 
+UNKNOWN_VERSION = Version(name="unknown", protocol=0)
+
 class ServerListing(BaseModel):
     name: str
     subdomain: str
-    version: Version
-    proxy: str
+    version: Version = UNKNOWN_VERSION
+    proxy: str | None
     sleep_properties: SleepProperties = SleepProperties()
     enabled: bool
+
+    # Annotate listing as valid or not
+    _errors: list[str] = PrivateAttr(default_factory=list)
+
+    @property
+    def valid(self):
+        return len(self._errors) == 0
+
+    @property
+    def errors(self):
+        return self._errors
 
 class Config(BaseModel):
     proxy_listing: list[ProxyListing]
     server_listing: list[ServerListing]
+
+    @model_validator(mode="after")
+    def validate_semantics(self):
+        proxy_names = set()
+        proxy_ports = set()
+        server_names = set()
+        server_subdomains = set()
+        for listing in self.proxy_listing:
+            if listing.name in proxy_names:
+                listing.errors.append(f"Proxy name \"{listing.name}\" is already taken")
+            if listing.port in proxy_ports:
+                listing.errors.append(f"Proxy port \"{listing.port}\" is already taken")
+            if listing.valid:
+                proxy_names.add(listing.name)
+                proxy_ports.add(listing.port)
+        for listing in self.server_listing:
+            if listing.name in server_names:
+                listing.errors.append(f"Server name \"{listing.name}\" is already taken")
+            if listing.subdomain in server_names:
+                listing.errors.append(f"Server subdomain \"{listing.subdomain}\" is already taken")
+            if listing.proxy not in proxy_names:
+                listing.errors.append(f"Server proxy \"{listing.proxy}\" does not exist")
+            if listing.valid:
+                server_names.add(listing.name)
+                server_subdomains.add(listing.subdomain)
+        return self
 
     @classmethod
     def default(cls):
